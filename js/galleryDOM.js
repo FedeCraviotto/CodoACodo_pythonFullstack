e49d082baef043d294c0eef2d98bc16164e785d6
FedeCraviotto <fcraviotto.dev@gmail.com>
2022-10-15 21:38:15 -0300

    Reorganizando carpetas

--- js/galleryDOM.js
+++ js/galleryDOM.js
@@ -6,7 +6,7 @@ imageURLs.forEach((imgLink, index) => { // Itero sobre el array de nombres de ar
     galleryImgContainer.classList.add('gallery-img__container', 'observedElement'); // Seteo la clase con el overflow=hidden;
     galleryImgContainer.innerHTML = //Le agrego el HTML de la imágen. Uso backticks y ${} para agregar js.
     ` 
-        <img src="images/seaLife/${imgLink}.jpg" alt="${imgLink}" class="img${index + 1} gallery-img">
+        <img src="../images/seaLife/${imgLink}.jpg" alt="${imgLink}" class="img${index + 1} gallery-img">
     `; // Cada nombre de imagen lo inserto en el path del src, en el alt, y uso el index para que cada imágen sea creada con una clase sea distinta 
     galleryContainer.append(galleryImgContainer); // Termina la vuelta y agrego el div con la img
 });
