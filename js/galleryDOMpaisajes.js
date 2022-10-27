a94ce9d53f94d82cb3abb308d6f1525e41de990c
FedeCraviotto <fcraviotto.dev@gmail.com>
2022-10-27 10:46:20 -0300

    Galería Paisajes grid listo v1

--- js/galleryDOMpaisajes.js
+++ js/galleryDOMpaisajes.js
@@ -10,7 +10,7 @@ for (let i = 1; i<= 9; i++) {
 }
 
 let imageURLsHorizonales = [];
-for (let i = 1; i<= 6; i++) {
+for (let i = 1; i<= 7; i++) {
     if (i<10) {
         imageURLsHorizonales.push(`PH0${i}.jpg`);
     } else {imageURLsHorizonales.push(`PH${i}.jpg`);
@@ -22,7 +22,7 @@ imageURLsHorizonales.forEach((imgLink, index) => {
     let galleryImgContainer = document.createElement('div'); 
     galleryImgContainer.classList.add('gallery-img__container-paisajes', 'observedImage', `imgH0${index + 1}`); 
     galleryImgContainer.innerHTML = 
-    `<img src="../images/paisajes/horizontal/${imgLink}" alt="${imgLink}" class="imgH0${index + 1} gallery-img">`; 
+    `<img src="../images/paisajes/horizontal/${imgLink}" alt="${imgLink}" class="gallery-img">`; 
     galleryContainerPaisajes.append(galleryImgContainer); 
 });
 
@@ -30,6 +30,6 @@ imageURLsVerticales.forEach((imgLink, index) => {
     let galleryImgContainer = document.createElement('div'); 
     galleryImgContainer.classList.add('gallery-img__container-paisajes', 'observedImage', `imgV0${index + 1}`); 
     galleryImgContainer.innerHTML = 
-    `<img src="../images/paisajes/vertical/${imgLink}" alt="${imgLink}" class="imgV0${index + 1} gallery-img">`; 
+    `<img src="../images/paisajes/vertical/${imgLink}" alt="${imgLink}" class="gallery-img">`; 
     galleryContainerPaisajes.append(galleryImgContainer); 
 });
