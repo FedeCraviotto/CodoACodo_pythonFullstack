505b74681d9d65c6bb1593d0b6ab7984939e53a2
FedeCraviotto <fcraviotto.dev@gmail.com>
2022-10-27 21:33:54 -0300

    Fetch y Slider OK

--- js/apiRequests.js
+++ js/apiRequests.js
@@ -1,10 +1,7 @@
+// fetch('https://www.rijksmuseum.nl/api/nl/collection?key=5PdmheO9&involvedMaker=Rembrandt+van+Rijn$imgonly=True&toppieces=True')
+// .then(response =>  response.json())
+// .then(data => {
+//     console.log(data.artObjects[0].longTitle);
+//     console.log(data.artObjects[0].webImage.url)
+// });
 
-// artObjects[0].headerImage.url
-
-// 5PdmheO9
-
-
-
-fetch('https://www.rijksmuseum.nl/api/nl/collection?key=5PdmheO9&involvedMaker=Rembrandt+van+Rijn')
-.then(response =>  response.json())
-.then(data => console.log(data));
